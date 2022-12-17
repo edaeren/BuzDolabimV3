cd2fbeba942d92a1d379f0f3e1a113ff4cbb10c4
edaeren <edaeren2001@outlook.com>
2022-12-17 17:25:25 +0300

    Menu %60 İlerleme- Slider Deneme %20 başarı.

---
+++ BuzDolabiV3/BuzDolabiV3/Controllers/TariflerController.cs
@@ -0,0 +1,32 @@
+﻿using BuzDolabiV3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BuzDolabiV3.Controllers
+{
+    public class TariflerController : Controller
+    {
+        private readonly ILogger<TariflerController> _logger;
+
+        public TariflerController(ILogger<TariflerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
